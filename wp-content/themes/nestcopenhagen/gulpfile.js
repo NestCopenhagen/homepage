34669757cf08c6fc48b3b14a2ca1026b860e1288
Andy Graulund <andreas@graulund.com>
2018-09-11 09:08:36 +0200

    Fixed styles, and styles infrastructure

--- wp-content/themes/nestcopenhagen/gulpfile.js
+++ wp-content/themes/nestcopenhagen/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')({ camelize: true });
- 
+
 // Styles
 gulp.task('styles', function() {
   return gulp.src('stylesheets/main.scss')
@@ -8,7 +8,7 @@ gulp.task('styles', function() {
     .pipe(plugins.sass({ style: 'expanded' }))
     .pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
     .pipe(gulp.dest('stylesheets/'))
-    .pipe(plugins.minifyCss({ keepSpecialComments: 1 }))
+    .pipe(plugins.cleanCss({ keepSpecialComments: 1 }))
     .pipe(plugins.rename('style.css'))
     .pipe(gulp.dest('./'))
     .pipe(plugins.notify({ message: 'Styles task complete' }));
@@ -18,7 +18,7 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
 
   gulp.watch('stylesheets/**/*.scss', ['styles']);
-  
+
 });
 
 gulp.task('default', ['styles', 'watch']);
